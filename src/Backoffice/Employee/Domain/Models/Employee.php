39d2c60b9b0e2a15a4cbbae6f8a837ea3a03f50f
gbauer-lightit <gaston.bauer@lightit.io>
2025-03-21 17:19:22 -0300

    feat: create migration and models for employees and tasks

---
+++ src/Backoffice/Employee/Domain/Models/Employee.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Backoffice\Employee\Domain\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Lightit\Backoffice\Task\Domain\Models\Task;
+
+final class Employee extends Model
+{
+    protected $guarded = ['id'];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+}
---
+++ src/Backoffice/Task/Domain/Models/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lightit\Backoffice\Task\Domain\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Lightit\Backoffice\Employee\Domain\Models\Employee;
+use Lightit\Backoffice\Task\Domain\Enums\TaskStatus;
+
+final class Task extends Model
+{
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'status' => TaskStatus::class,
+    ];
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
+}
