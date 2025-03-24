c3f18618a174e8d1b0ac2a2a565db459eaad777c
gbauer-lightit <gaston.bauer@lightit.io>
2025-03-24 11:13:47 -0300

    feat: update response handling in employee and task controllers to return JSON format

--- src/Backoffice/Employee/App/Controllers/ListEmployeeController.php
+++ src/Backoffice/Employee/App/Controllers/ListEmployeeController.php
@@ -9,7 +9,7 @@ use Lightit\Backoffice\Employee\Domain\Actions\ListEmployeeAction;
 
 final class ListEmployeeController
 {
-    public function __invoke(ListEmployeeAction $listEmployeeAction): JsonResponse
+    public function __invoke(): JsonResponse
     {
         return response()->json([
             'message' => 'List of employees',
