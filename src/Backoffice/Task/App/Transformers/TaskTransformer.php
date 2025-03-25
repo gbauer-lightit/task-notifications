59310ee73fb155ca104854aaf633c9ede218639a
gbauer-lightit <gaston.bauer@lightit.io>
2025-03-25 11:19:14 -0300

    feat: rename employee and task controllers and actions for consistency

--- src/Backoffice/Task/App/Transformers/TaskTransformer.php
+++ src/Backoffice/Task/App/Transformers/TaskTransformer.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Lightit\Backoffice\Task\App\Transformers;
 
 final class TaskTransformer
 {
-
 }
--- src/Backoffice/Task/Domain/Actions/ListTasksAction.php
+++ src/Backoffice/Task/Domain/Actions/ListTasksAction.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Lightit\Backoffice\Task\Domain\Actions;
 
 final class ListTasksAction
 {
-
 }
